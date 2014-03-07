a9929a07aead4ce0bf8459f5c07098f0c184304a
HHoofs <huubhoofs@gmail.com>
2014-03-07 12:55:32 +0100

    Bestand om de terms (zowel functies als terms) te achterhalen zodat deze te plakken zijn!

---
+++ SearchTermsFunc.R
@@ -0,0 +1,28 @@
+require(stringr)
+Chapter1 <- file("Chapter1.Rnw", "rt") 
+
+Term_w <- str_detect(string=Chapter1, pattern="\\\\term")
+Term <- list()
+for(i in 1:length(Chapter1[Which])){
+line <- Chapter1[Term_w][i]
+line_cleaned <- gsub("[\\{\\}]", "", regmatches(line, gregexpr("term\\{.*?\\}", line))[[1]])
+line_term_cleaned <- gsub("term","",line_cleaned)
+Term[[i]] <- line_term_cleaned
+}
+
+cat(unique(unlist(Term)),sep=",")
+
+
+Func_w <- str_detect(string=Chapter1, pattern="\\\\func")
+Func <- list()
+for(i in 1:length(Chapter1[Func_w])){
+line <- Chapter1[Func_w][i]
+line_cleaned <- gsub("[\\{\\}]", "", regmatches(line, gregexpr("func\\{.*?\\}", line))[[1]])
+line_term_cleaned <- gsub("func","",line_cleaned)
+Func[[i]] <- line_term_cleaned
+}
+
+cat(unique(unlist(Func)),sep=",")
+
+closeAllConnections()
+
